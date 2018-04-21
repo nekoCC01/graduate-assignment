865f6533dbd3dfe59cc08cd1c7fe24278dea7db3
Daniel Kasai <danielkasai2012@gmail.com>
2018-04-22 00:43:10 +0200

    finishing the presentation

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -4,18 +4,15 @@ var router = express.Router();
 var Director = require('../models/directorModel');
 var Movie = require('../models/movieModel');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  Movie.
-      find({}).
-      populate('director').
+  Movie.find({}).populate('director').
       exec(function(err, movies) {
-        if (err) {
-          console.log(err);
-        }
-        console.log(movies);
-        res.render('index', {movies: movies});
-      });
+          if (err) {
+            console.log(err);
+          }
+          res.render('index', {movies: movies});
+      }
+  );
 });
 
 module.exports = router;
